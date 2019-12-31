b05a9b82842fbc6e99b7081d2489228ca0e3ead3
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2019-12-31 14:44:38 +0530

    String Concatenation @ C#

---
+++ C#/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John ";
+            string lastName = "Doe";
+            string name = firstName + lastName;
+            Console.WriteLine(name);
+        }
+    }
+}
