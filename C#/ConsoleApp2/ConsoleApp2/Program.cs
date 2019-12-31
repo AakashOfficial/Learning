2bf9aea6de20cd87d284f553d553948ae8e371b3
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2019-12-31 14:32:02 +0530

    String Length

---
+++ C#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
+        }
+    }
+}
