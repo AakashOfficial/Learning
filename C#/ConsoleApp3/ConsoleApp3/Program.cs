b1144da26b34613c6671fe10c26e7023bdc0d79b
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2019-12-31 14:35:23 +0530

    Upper and Lower Care @ C#

---
+++ C#/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "Hello World";
+            Console.WriteLine(txt.ToUpper());
+            Console.WriteLine(txt.ToLower());
+        }
+    }
+}
