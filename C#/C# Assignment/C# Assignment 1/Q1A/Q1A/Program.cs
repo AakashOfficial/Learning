7c53e15103d264086a303bcb6d6578b1dc007ef5
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2020-01-02 17:09:57 +0530

    Assignment for Int Conversion

---
+++ C#/C# Assignment/C# Assignment 1/Q1A/Q1A/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Q1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taking the input from user for conversion
+            System.Console.WriteLine("Enter the Value to be Convert");
+            string InputValue = System.Console.ReadLine() ;
+
+            // Convert by int.parse() method
+            int ConvertedValue = int.Parse(InputValue);
+            System.Console.WriteLine("Input Converted Using the int.Parse() method") ;
+            System.Console.WriteLine(ConvertedValue);
+
+            // Convert by the Convert.ToInt method
+            ConvertedValue = System.Convert.ToInt32(InputValue);
+            System.Console.WriteLine("Input Converted Using the Convert.ToInt() method");
+            System.Console.WriteLine(ConvertedValue);
+
+            // For the use int the int.TryParse() method
+            int DefaultValue = -1;
+            bool ConvertedValueResult;
+            ConvertedValueResult = int.TryParse(InputValue , out DefaultValue);
+            System.Console.WriteLine("Input Converted Using the Convert.ToInt() method");
+            System.Console.WriteLine(ConvertedValueResult);
+
+        }
+    }
+}
