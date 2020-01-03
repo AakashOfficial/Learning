4900c061393a16bcf8425aaf5098f2170b3aa441
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2020-01-03 11:08:05 +0530

    Assignment for Float Conversion

---
+++ C#/C# Assignment/C# Assignment 1/Q2B/Q2B/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Q2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Show message to user
+            System.Console.WriteLine("Enter Input to be Converted");
+
+            // taking Input from the User
+            string InputValue = System.Console.ReadLine();
+
+            // Converted using the float.Parse() method
+            float ConvertedValue = float.Parse(InputValue);
+            System.Console.WriteLine("Converted using the float.Parse() method");
+            System.Console.WriteLine("Output : " + ConvertedValue);
+
+            // Converted using the float.TryParse() method
+            float def = 2.0F;
+            bool ConvertedValuetryParse = float.TryParse(InputValue, out def);
+            System.Console.WriteLine("Converted using the float.TryParse() method");
+            System.Console.WriteLine("Output : " + ConvertedValuetryParse);
+
+
+        }
+    }
+}
