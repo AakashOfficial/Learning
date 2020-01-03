497568c4ae3b33311a57f99c633c2f87c473a897
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2020-01-03 11:15:28 +0530

    Assignment for Boolean Conversion

---
+++ C#/C# Assignment/C# Assignment 1/Q1C/Q1C/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Q1C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Show message to user
+            System.Console.WriteLine("Enter Input to be Converted");
+
+            // taking Input from the User
+            string InputValue = System.Console.ReadLine();
+
+            // Converted using the bool.Parse() method
+            bool ConvertedValue = bool.Parse(InputValue);
+            System.Console.WriteLine("Converted using the bool.Parse() method");
+            System.Console.WriteLine("Output : " + ConvertedValue);
+
+            // Converted using the bool.TryParse() method
+            bool def = true;
+            ConvertedValue = bool.TryParse(InputValue, out def);
+            System.Console.WriteLine("Converted using the bool.TryParse() method");
+            System.Console.WriteLine("Output : " + ConvertedValue);
+        }
+    }
+}
