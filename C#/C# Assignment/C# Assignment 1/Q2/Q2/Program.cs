3af39db0f10572f9e1292aa7e916c94540092e8e
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2020-01-07 10:40:49 +0530

    Assignment for Compare between ==, object.Equals, object.ReferenceEquals

--- C#/C# Assignment/C# Assignment 1/Q2/Q2/Program.cs
+++ C#/C# Assignment/C# Assignment 1/Q2/Q2/Program.cs
@@ -7,12 +7,34 @@ namespace Q2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+            // input parameters using in the program
             string param = null;
             int param2 = 1;
             float param3 = 1.0F;
 
+
+            // call CheckIntEqual()
+            CheckIntEqual(param2, param3);
+
+            // call CheckIntReferenceEquals()
+            CheckIntReferenceEquals();
+
+            // call CheckIntEquals()
+            CheckIntEquals(param2, param3);
+
+            // call CheckStringEqual()
+            CheckStringEqual(param);
+
+            // call CheckStringReferenceEquals()
+            CheckStringReferenceEquals(param);
+
+            // call CheckStringEquals()
+            CheckStringEquals(param);
+        }
+
+        // method For check behaviour of == with int
+        static void CheckIntEqual(int param2, float param3) {
             // Checking the behaviour of == for integers
             // Outcome : Values should be Same
             // return true if equals otherwise false
@@ -21,16 +43,10 @@ namespace Q2
             } else {
                 System.Console.WriteLine("False");
             }
+        }
 
-            // Checking the behaviour of Equals for integers
-            // Outcome : both the parameters should be equal and same length
-            // return true if equals otherwise false
-            if (param2.Equals(param3)) {
-                System.Console.WriteLine(param2.Equals(param3));
-            } else {
-                System.Console.WriteLine("False");
-            }
-
+        // method For check behaviour of object.ReferenceEquals with int
+        static void CheckIntReferenceEquals() {
             // Checking the behaviour of refrenceequals for integers
             // Outcome : Checking the Reference of the Objects
             // return true if equals otherwise false
@@ -39,8 +55,23 @@ namespace Q2
             } else {
                 System.Console.WriteLine("False");
             }
+        }
 
-            // Checking the behaviour of refrenceequals for String
+        // method For check behaviour of Equals with int
+        static void CheckIntEquals(int param2, float param3) {
+            // Checking the behaviour of Equals for integers
+            // Outcome : both the parameters should be equal and same length
+            // return true if equals otherwise false
+            if (param2.Equals(param3)) {
+                System.Console.WriteLine(param2.Equals(param3));
+            } else {
+                System.Console.WriteLine("False");
+            }
+        }
+
+        // method For check behaviour of == with strings
+        static void CheckStringEqual(string param) {
+            // Checking the behaviour of == for String
             // Outcome : Checking the Values.
             // return true if equals otherwise false
             if (param == null) {
@@ -48,7 +79,10 @@ namespace Q2
             } else {
                 System.Console.WriteLine("False");
             }
+        }
 
+        // method For check behaviour of object.ReferenceEquals with strings
+        static void CheckStringReferenceEquals(string param) {
             // Checking the behaviour of refrenceequals for String
             // Outcome : Checking the Reference of the Objects
             // return true if equals otherwise false
@@ -57,7 +91,10 @@ namespace Q2
             } else {
                 System.Console.WriteLine("False");
             }
+        }
 
+        // method For check behaviour of Equals with strings
+        static void CheckStringEquals(string param) {
             // Checking the behaviour of refrenceequals for String
             // Outcome : Checking the Reference of the Objects
             // return true if equals otherwise false
@@ -66,7 +103,7 @@ namespace Q2
             } else {
                 System.Console.WriteLine("False");
             }
-            // Output : Exception because Reference of Object not be Null
+            // Output : Exception because Reference of Object not be Null}
         }
     }
 }
