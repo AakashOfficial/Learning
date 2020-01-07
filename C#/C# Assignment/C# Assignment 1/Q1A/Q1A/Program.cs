b3497baf6be2c1927ef24cfa86c1673a61756889
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2020-01-07 10:07:00 +0530

    Assignment for Int Conversion

--- C#/C# Assignment/C# Assignment 1/Q1A/Q1A/Program.cs
+++ C#/C# Assignment/C# Assignment 1/Q1A/Q1A/Program.cs
@@ -6,29 +6,55 @@ namespace Q1A
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+
+            // call TakeInput() for input
+            string InputValue = TakeInput();
+
+            // Call ConvertIntParse() 
+            ConvertIntParse(InputValue);
+
+            // Call ConvertIntToInt()
+            ConvertIntToInt(InputValue);
+
+            // call ConvertIntTryParse()
+            ConvertIntTryParse(InputValue);
+        }
+
+        // function for taking the input from user and return
+        static string TakeInput() {
             // Taking the input from user for conversion
             System.Console.WriteLine("Enter the Value to be Convert");
-            string InputValue = System.Console.ReadLine() ;
+            string InputValue = System.Console.ReadLine();
 
+            return InputValue;
+        }
+
+        // function for convert to int by int.parse()
+        static void ConvertIntParse(string InputValue) {
             // Convert by int.parse() method
             int ConvertedValue = int.Parse(InputValue);
-            System.Console.WriteLine("Input Converted Using the int.Parse() method") ;
+            System.Console.WriteLine("Input Converted Using the int.Parse() method");
             System.Console.WriteLine(ConvertedValue);
+        }
 
+        // function for convert to int by Convert.ToInt()
+        static void ConvertIntToInt(string InputValue) {
             // Convert by the Convert.ToInt method
-            ConvertedValue = System.Convert.ToInt32(InputValue);
+            int ConvertedValue = System.Convert.ToInt32(InputValue);
             System.Console.WriteLine("Input Converted Using the Convert.ToInt() method");
             System.Console.WriteLine(ConvertedValue);
+        }
 
+        // function for convert to int by int.TryParse()
+        static void ConvertIntTryParse(string InputValue) {
             // For the use int the int.TryParse() method
             int DefaultValue = -1;
             bool ConvertedValueResult;
-            ConvertedValueResult = int.TryParse(InputValue , out DefaultValue);
+            ConvertedValueResult = int.TryParse(InputValue, out DefaultValue);
             System.Console.WriteLine("Input Converted Using the Convert.ToInt() method");
             System.Console.WriteLine(ConvertedValueResult);
-
         }
+        
     }
 }
