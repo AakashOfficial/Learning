ecb10815a3a6dba5f52ffbca702c64ddb8516949
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2020-01-09 14:14:46 +0530

    CRUD on Equipment Extend

---
+++ C#/C# Assignment/C# Assignment 3/Q1/Q1/Equipment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EquipmentAssignmentExtend {
+ 
+    /**
+     * it is a abstract Equipment class 
+     * which takes equipmentName, inputDistance,public string description,distanceMoved, maintenanceCost 
+     * as input and have two function getdata and showdata and one abstract  update distance funtion
+     */
+
+    public enum equipmentType {
+        Mobile = 1,
+        Immobile = 2
+    };
+
+    public abstract class Equipment {
+
+        // parameters for Equipment Class
+        public string equipmentName;
+        public int inputDistance;
+        public string description;
+        public int distanceMoved;
+        public int maintenanceCost;
+        public equipmentType eT;
+
+        // Both getdata and updatedistance are abstract function
+        public abstract void GetData();
+        public abstract void UpdateDistance(int addDistance);
+
+        // this functionUsed To take input of equipment name and equipment distance
+        public void GetInput() {
+            Console.WriteLine("Enter Name of Equipment");
+            equipmentName = Console.ReadLine();
+            Console.WriteLine("Enter Description of Equipment " + equipmentName);
+            description = Console.ReadLine();
+        }
+
+        // this function showoutput shows all the detail like name ,description,distancemoved
+        public void ShowOutput() {
+            Console.WriteLine("Name of Equipment : " + equipmentName);
+            Console.WriteLine("Description of Equipment '" + equipmentName + "' : " + description);
+            Console.WriteLine("Total Distance Moved by Equipment '" + equipmentName  + "' : " + this.distanceMoved);
+            Console.WriteLine("Total  Maintenance Cost of Equipment '" + equipmentName  + "' : " + this.maintenanceCost);
+        }
+
+        internal void UpdateDistance() {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ C#/C# Assignment/C# Assignment 3/Q1/Q1/EquipmentMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EquipmentAssignmentExtend {
+
+    class EquipmentMain {
+
+        static void Main(string[] args) {
+           
+            // varible used to calculate no of equipment
+            int mobileCount = 0;
+            int immobileCount = 0;
+            int selectEquipment;
+            string temp;
+
+            // object through dynameic slicing
+            Equipment equipment1 = new Mobile();
+            Equipment equipment2 = new Immobile();
+
+            //to check whether used does not input wrong input
+            do {
+                var equipmentTypeValue1 = equipmentType.Mobile;
+                var equipmentTypeValue2 = equipmentType.Immobile;
+                Console.WriteLine("Enter Type of Equipment");
+                while (true) // check whether input is correct
+                {
+                    Console.WriteLine("Press 1 fo Mobile and 2 for ImMobile");
+                    selectEquipment = Convert.ToInt32(Console.ReadLine());
+                    if (selectEquipment == (int)equipmentTypeValue1) {
+                        equipment1.GetInput();
+                        equipment1.GetData();
+                        equipment1.UpdateDistance(equipment1.inputDistance);
+                        equipment1.ShowOutput();
+                        mobileCount++;
+                        break;
+                    }else if (selectEquipment == (int)equipmentTypeValue2) {
+                        equipment2.GetInput();
+                        equipment2.GetData();
+                        equipment2.UpdateDistance(equipment2.inputDistance);
+                        equipment2.ShowOutput();
+                        immobileCount++;
+                        break;
+                    } else {
+                        Console.WriteLine("Enter Again");
+                        continue;
+                    }
+                }
+
+                Console.WriteLine("Total No of Mobile equipment " + mobileCount);
+                Console.WriteLine("Total No of imMobile equipment " + immobileCount);
+                Console.WriteLine("Do you wnat to continue [y/n]");
+                temp = Console.ReadLine();
+            } while (temp == "y");
+
+        }
+    }
+}
