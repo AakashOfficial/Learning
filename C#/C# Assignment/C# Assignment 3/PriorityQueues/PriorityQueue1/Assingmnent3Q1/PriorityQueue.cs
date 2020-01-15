7e0d97911415a27aa8b688176995a326f6a346c4
aakashtyagi1996@outlook.com <aakashtyagi1996@outlook.com>
2020-01-15 10:25:02 +0530

    Priority Queue Way 1

---
+++ C#/C# Assignment/C# Assignment 3/PriorityQueues/PriorityQueue1/Assingmnent3Q1/PriorityQueue.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PriorityQueue {
+    
+    // all the function of priority queue is implemented in this Class
+    class PriorityQueue<T>   where T : IEquatable<T> {
+        public int count;
+        private IDictionary<int, IList<T>> elements;
+        IList<T> list;
+        T temp;
+        
+        //constructor 
+        public PriorityQueue() {
+            elements = new Dictionary<int, IList<T>>();
+            list = new List<T>();
+        }
+
+        public PriorityQueue(IDictionary<int,List<T>> element):this () {
+            elements = new Dictionary<int, IList<T>>();
+            list = new List<T>();
+        }
+
+        //enqueue method for insertion of data
+        public void Enqueue(int priority, T item) {
+            if (!elements.TryGetValue(priority, out list)) {
+                list = new List<T>();
+                elements.Add(priority, list);
+            }
+            list.Add(item);
+            count++;
+        }
+
+        //check whether element is prsent or not
+        public bool Contains(T item) {
+            bool isContain = false;
+            foreach (KeyValuePair<int, IList<T>> kvp in elements) {
+                foreach (var value in kvp.Value) {
+                    if (item.Equals(value)) {
+                        return true;
+                    }
+                }
+            }
+            return isContain;
+        }
+
+        //remove the element from the priority queue
+        public int Dequeue() {
+            if (elements.Count == 0)
+                throw new Exception("No items to Dequeue:");
+                var keys = GetHighestPriority();
+                elements[keys].RemoveAt(0);
+                count--;
+                return keys;
+        }       
+        
+        // print the priority queue
+        public void Print() {
+            foreach (KeyValuePair<int, IList<T>> kvp in elements) {
+                foreach(var value in kvp.Value) {
+                    Console.WriteLine("key = {0} value = {1} ", kvp.Key,value);
+                }
+            }
+        }
+
+        //print the top highest element in priority queue
+        public T Peek() {
+            if (count == 0) throw new InvalidOperationException("Queue is empty.");
+
+            foreach (KeyValuePair<int, IList<T>> kvp in elements) {
+                foreach (var value in kvp.Value) {
+                    temp=value;
+                    break;
+                }
+                break;
+            }
+            return temp;
+        }
+
+        //print highest priority form queue
+        private int GetHighestPriority() {
+            int priority = 0;
+            foreach (KeyValuePair<int, IList<T>> kvp in elements) {
+                if (priority < kvp.Key) {
+                    priority = kvp.Key; 
+                }
+            }
+            return priority;
+        }
+    }
+}
---
+++ C#/C# Assignment/C# Assignment 3/PriorityQueues/PriorityQueue1/Assingmnent3Q1/QueueMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PriorityQueue {
+
+    class QueuemMain {
+
+        // it is main method in which we call all function of pritority queue
+        static void Main(string[] args) {
+
+            int priorityElement;
+            int key;
+            int value;
+            int takeInput;
+            string continueCheck;
+            PriorityQueue<int> processQueue = new PriorityQueue<int>();
+            PriorityQueue<int> processQueue1 = new PriorityQueue<int>( new Dictionary<int,List<int>>());
+
+            do {
+                Console.WriteLine("press 1 to insert a element in priorit queue");
+                Console.WriteLine("press 2 to remove a element in priorit queue");
+                Console.WriteLine("press 3 to check whether  a element is in priorit queue or not");
+                Console.WriteLine("press 4 to know the peek  element in priorit queue");
+                //to perform which operation of queue
+                takeInput = Convert.ToInt32(Console.ReadLine());
+                switch (takeInput) {
+                    //to insert element in queue
+                    case 1:
+                        Console.WriteLine("Enter the number of element you want to insert in a  priority queue");
+                        priorityElement = Convert.ToInt32(Console.ReadLine());
+                        for (int number = 0; number < priorityElement; number++) {
+                            Console.WriteLine("Enter the item you want to insert");
+                            key = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter the priority of the element");
+                            value = Convert.ToInt32(Console.ReadLine());
+                            processQueue.Enqueue(key, value);
+                        }
+                        processQueue.Print();
+                        break;
+                    //to remove fro queue
+                    case 2:
+                        try {
+                            processQueue.Dequeue();
+                            processQueue.Print();
+                        } catch (Exception e) {
+                            Console.WriteLine("Cannot remove more element because of " + e.Message);
+                        }
+
+                        break;
+                    //to check whether element is present or not
+                    case 3:
+                        Console.WriteLine("Enter the element you want to search");
+                        priorityElement = Convert.ToInt32(Console.ReadLine());
+                        bool present = processQueue.Contains(priorityElement);
+                        if (present == true) {
+                            Console.WriteLine("Element present");
+                        } else {
+                            Console.WriteLine("Element not present");
+                        }
+                        break;
+                    //to check which element is present on the top of queue
+                    case 4:
+                        try {
+                            Console.WriteLine("Element at the top is  " + processQueue.Peek());
+                        } catch (Exception e) {
+                            Console.WriteLine("Cannot find peek elemennt due to " + e.Message);
+                        }
+                            break;
+                    default:
+                        Console.WriteLine("Enter Again");
+                        break;
+                }
+                Console.WriteLine("Do you wan to perfrom more operation [y/n]");
+                continueCheck = Console.ReadLine();
+            } while (continueCheck == "y");
+        }
+    }
+}
