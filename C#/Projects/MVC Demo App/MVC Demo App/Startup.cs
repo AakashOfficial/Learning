4be205c9f5f16fadc7871a36ea99008a1f872779
AakashOfficial <aakashtyagi1996@outlook.com>
2020-01-22 14:13:36 +0530

    Demo App

---
+++ C#/Projects/MVC Demo App/MVC Demo App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Demo_App.Startup))]
+namespace MVC_Demo_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
