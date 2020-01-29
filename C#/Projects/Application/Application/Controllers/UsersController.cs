61a9909bf70d505be034913f2d81e0a739a86413
AakashOfficial <aakashtyagi1996@outlook.com>
2020-01-29 10:21:54 +0530

    Create Form

--- C#/Projects/Application/Application/Controllers/UsersController.cs
+++ C#/Projects/Application/Application/Controllers/UsersController.cs
@@ -19,38 +19,6 @@ namespace Application.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Create(UserModel account)
-        {
-            if (ModelState.IsValid)
-            {
-                using (CrudContext db = new CrudContext())
-                {
-                    db.user.Add(account);
-                    db.SaveChanges();
-                }
-
-                ModelState.Clear();
-                
-                ViewBag.Message = account.UserName + " successfully created.";
-            }
-            return View();
-        }
-
-        public ActionResult Retrive(UserModel  user)
-        {
-
-            using (CrudContext db = new CrudContext())
-            {
-                var output = db.user.ToList();
-                return View(output);
-            }
-
-        }
-
-        public ActionResult Updateuser()
-        {
-            return View();
-        }
+        
     }
 }
