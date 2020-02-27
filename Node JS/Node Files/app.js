d001456e35aa91dc191915b3784ba176d3a42089
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-27 11:47:38 +0530

    Create from console done

---
+++ Node JS/Node Files/app.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+
+console.log("Welcome");
+
+console.log("Enter File Name");
+
+var fileName = process.argv[2];
+
+if (fileName == '' || fileName == null) {
+    console.log("You Not Enter Correct File Name");
+} else {
+    console.log("Please Enter Choice.");
+    console.log("1. Create file and write to it.");
+    console.log("2. Override the File.");
+    console.log("3. Append the File.");
+    console.log("4. Read the File.");
+
+    var fileOperation = process.argv[3];
+    // console.log(process.argv[3]);
+    if (fileOperation == "1") {
+        console.log("Enter the Content to write to the File.");
+        var content = process.argv[4];
+        // console.log(process.argv[4]);
+        if (content != '') {
+            fs.writeFileSync(fileName, content);
+        }
+    }
+}
